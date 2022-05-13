2660a7b6f19869123c1d0f2e45fb5bcdca12a448
Dmitriy Pozdin <dmitriy.pozdin@bia-tech.ru>
2022-05-13 17:09:52 +0300

    basic layout view with buttons, total value and grid

--- app/src/main/java/com/podmev/cashsplitter/MainActivity.kt
+++ app/src/main/java/com/podmev/cashsplitter/MainActivity.kt
@@ -9,17 +9,27 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
 import com.podmev.cashsplitter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    var data = MutableList<String>(31, {x->"$x"})
+    lateinit var adapter: ArrayAdapter<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data)
+        binding.gridCategories.adapter = adapter
 
     }
+
+
 
 }
