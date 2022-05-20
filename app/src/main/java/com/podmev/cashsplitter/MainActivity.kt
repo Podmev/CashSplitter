18f830833293f7ade0ef3d4aeb154915ce76dce2
Dmitriy Pozdin <Dmitriy.Pozdin@bia-tech.ru>
2022-05-20 15:00:04 +0300

    optimized imports in MainActivity

--- app/src/main/java/com/podmev/cashsplitter/MainActivity.kt
+++ app/src/main/java/com/podmev/cashsplitter/MainActivity.kt
@@ -1,28 +1,12 @@
 package com.podmev.cashsplitter
 
-import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.util.TypedValue
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.podmev.cashsplitter.data.*
 import com.podmev.cashsplitter.databinding.ActivityMainBinding
-import com.podmev.cashsplitter.dialogs.EditTextDialog
-import com.podmev.cashsplitter.dialogs.NumberEditTextDialog
-import com.podmev.cashsplitter.dialogs.SimpleDialog
-import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
